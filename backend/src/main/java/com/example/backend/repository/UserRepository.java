f23cdfd306e872831367c58ab1df22a4e8af149e
Le Tuan Binh <ltbinh21@clc.fitus.edu.vn>
2024-12-21 15:14:11 +0700

    Clean the function register because it will pass into another team member

--- backend/src/main/java/com/example/backend/repository/UserRepository.java
+++ backend/src/main/java/com/example/backend/repository/UserRepository.java
@@ -3,5 +3,6 @@ package com.example.backend.repository;
 import com.example.backend.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface UserRepository extends JpaRepository<User, Integer> {
 }

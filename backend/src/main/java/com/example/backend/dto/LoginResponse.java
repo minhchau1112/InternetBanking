d2ac74f9c999510a15afe49a60a1ca58af7f3e65
Le Tuan Binh <ltbinh21@clc.fitus.edu.vn>
2024-12-21 15:27:38 +0700

    Create Login Response and Login Request

---
+++ backend/src/main/java/com/example/backend/dto/LoginResponse.java
@@ -0,0 +1,19 @@
+package com.example.backend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class LoginResponse {
+    private Integer id;
+    private String username;
+    private String role;
+    private String accessToken;
+    private String refreshToken;
+    private Integer expiresIn;
+}
--- backend/src/main/java/com/example/backend/repository/UserRepository.java
+++ backend/src/main/java/com/example/backend/repository/UserRepository.java
@@ -2,7 +2,8 @@ package com.example.backend.repository;
 
 import com.example.backend.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByUsername(String username);
 }

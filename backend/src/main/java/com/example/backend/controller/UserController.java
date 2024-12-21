336dc69085533ef4a78e6ed6fb139b8b9f059e78
Le Tuan Binh <ltbinh21@clc.fitus.edu.vn>
2024-12-21 23:40:55 +0700

    Fake the new user by create user function

--- backend/src/main/java/com/example/backend/controller/UserController.java
+++ backend/src/main/java/com/example/backend/controller/UserController.java
@@ -1,8 +1,15 @@
 package com.example.backend.controller;
 
+import com.example.backend.model.User;
 import com.example.backend.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 public class UserController {
@@ -15,5 +22,12 @@ public class UserController {
         this.passwordEncoder = passwordEncoder;
     }
 
+    @PostMapping("/users")
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        String hash = passwordEncoder.encode(user.getPassword());
+        user.setPassword(hash);
+        User createdUser = userService.handleCreateUser(user);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdUser);
+    }
 
 }
--- backend/src/main/java/com/example/backend/service/UserService.java
+++ backend/src/main/java/com/example/backend/service/UserService.java
@@ -22,5 +22,13 @@ public class UserService {
         return this.userRepository.findByUsername((username));
     }
 
+    public User handleCreateUser(User user) {
+        Optional<User> existingUser = userRepository.findByUsername(user.getUsername());
+        if (existingUser.isPresent()) {
+            throw new IllegalArgumentException("User with username " + user.getUsername() + " already exists.");
+        }
+        user.setCreatedAt(LocalDateTime.now());
+        return userRepository.save(user);
+    }
 
 }

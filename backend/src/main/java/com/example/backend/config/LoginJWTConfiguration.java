f1a5be6af474af4ad30e5ceaae927e479e5b555a
Le Tuan Binh <ltbinh21@clc.fitus.edu.vn>
2024-12-25 04:05:33 +0700

    Update the login service

--- backend/src/main/java/com/example/backend/config/LoginJWTConfiguration.java
+++ backend/src/main/java/com/example/backend/config/LoginJWTConfiguration.java
@@ -45,7 +45,7 @@ public class LoginJWTConfiguration {
     public JwtAuthenticationConverter jwtAuthenticationConverter() {
         JwtGrantedAuthoritiesConverter grantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
         grantedAuthoritiesConverter.setAuthorityPrefix("");
-        grantedAuthoritiesConverter.setAuthoritiesClaimName("role");
+        grantedAuthoritiesConverter.setAuthoritiesClaimName("user");
 
         JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
         jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(grantedAuthoritiesConverter);
--- backend/src/main/java/com/example/backend/controller/UserController.java
+++ backend/src/main/java/com/example/backend/controller/UserController.java
@@ -1,9 +1,12 @@
 package com.example.backend.controller;
 
 import com.example.backend.model.User;
+import com.example.backend.service.LoginService;
 import com.example.backend.service.UserService;
+import org.apache.catalina.security.SecurityUtil;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -35,6 +38,11 @@ public class UserController {
     @GetMapping("/users")
     public ResponseEntity<List<User>> getAllUser() {
         return ResponseEntity.status(HttpStatus.OK).body(this.userService.fetchAllUsers());
+    }
+
+    @GetMapping("/auth/user")
+    public ResponseEntity<User> getAuthUser() {
+        String username =
     }
 
 }

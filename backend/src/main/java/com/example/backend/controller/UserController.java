1b39e95707cffe0bbe810be5a84ff7aa7ea67c12
Le Tuan Binh <ltbinh21@clc.fitus.edu.vn>
2024-12-25 04:21:05 +0700

    Update get current user by access_token

--- backend/src/main/java/com/example/backend/controller/UserController.java
+++ backend/src/main/java/com/example/backend/controller/UserController.java
@@ -1,12 +1,16 @@
 package com.example.backend.controller;
 
-import com.example.backend.model.User;
+import com.example.backend.dto.response.LoginResponse;
+import com.example.backend.model.*;
+import com.example.backend.service.AccountService;
 import com.example.backend.service.LoginService;
 import com.example.backend.service.UserService;
+import jakarta.persistence.EntityNotFoundException;
 import org.apache.catalina.security.SecurityUtil;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,10 +25,12 @@ public class UserController {
 
     private final UserService userService;
     private final PasswordEncoder passwordEncoder;
+    private final AccountService accountService;
 
-    public UserController(UserService userService, PasswordEncoder passwordEncoder) {
+    public UserController(UserService userService, PasswordEncoder passwordEncoder, AccountService accountService) {
         this.userService = userService;
         this.passwordEncoder = passwordEncoder;
+        this.accountService = accountService;
     }
 
     @PostMapping("/users")
@@ -40,9 +46,36 @@ public class UserController {
         return ResponseEntity.status(HttpStatus.OK).body(this.userService.fetchAllUsers());
     }
 
-    @GetMapping("/auth/user")
-    public ResponseEntity<User> getAuthUser() {
-        String username =
+    @GetMapping("/api/auth/user")
+    public ResponseEntity<LoginResponse.UserInformation> getCurrentUser() {
+        String username = LoginService.getCurrentUserLogin().isPresent() ? LoginService.getCurrentUserLogin().get() : "";
+
+        User user = userService.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        LoginResponse.UserInformation userInformation = new LoginResponse.UserInformation();
+        String role = getRole(user);
+        userInformation.setRole(role);
+        if (user!=null) {
+            userInformation.setUsername(user.getUsername());
+            if (role=="ROLE_CUSTOMER"){
+                Account account = accountService.findByCustomerId(user.getId())
+                        .orElseThrow(() -> new EntityNotFoundException("Account not found"));
+                userInformation.setAccountID(account.getId());
+            }
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(userInformation);
+    }
+    public String getRole(User user) {
+        if (user instanceof Admin) {
+            return "ROLE_ADMIN";
+        }
+        if (user instanceof Customer) {
+            return "ROLE_CUSTOMER";
+        }
+        if (user instanceof Employee) {
+            return "ROLE_EMPLOYEE";
+        }
+        throw new IllegalStateException("Unknown user type: " + user.getClass().getName());
     }
 
 }

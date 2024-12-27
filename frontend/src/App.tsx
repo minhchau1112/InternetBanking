77d147f29e0aa0e7692353883a3d46206b63bf24
Le Tuan Binh <ltbinh21@clc.fitus.edu.vn>
2024-12-27 21:19:10 +0700

    Split into api ts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,22 +1,23 @@
 import { Routes, Route } from "react-router-dom";
 import CustomerDashboard from "./pages/customer/CustomerDashboard";
 import Sidebar from "./components/Sidebar";
+import {LoginForm} from "@/pages/login/Login.tsx";
 
 function App() {
-  const userType = "employee";
-
-  return (
-    <div className="flex">
-      <Sidebar userType={userType} />
-      <div className="flex-grow">
-        <Routes>
-          <Route path="/" element={<h1>Welcome to Internet Banking</h1>} />
-          <Route path="/customer" element={<CustomerDashboard />} />
-          {/*<Route path="/customers" element={<AccountCreation/>} />*/}
-        </Routes>
-      </div>
-    </div>
-  );
+    const userType = "employee";
+    const isLoginPage = location.pathname === "/login";
+    return (
+        <div className="flex">
+            {!isLoginPage && <Sidebar userType={userType} />}
+            <div className={`flex-grow ${isLoginPage ? "" : "ml-64"}`}>
+                <Routes>
+                    <Route path="/" element={<h1>Welcome to Internet Banking</h1>} />
+                    <Route path="/customer" element={<CustomerDashboard />} />
+                    <Route path="/login" element={<LoginForm />} />
+                </Routes>
+            </div>
+        </div>
+    );
 }
 
 export default App;
--- frontend/src/pages/login/Login.tsx
+++ frontend/src/pages/login/Login.tsx
@@ -1,40 +1,43 @@
-import React, { useState } from "react";
-import { Box, Button, TextField, Typography, IconButton, InputAdornment, Link } from "@mui/material";
-import { Visibility, VisibilityOff } from "@mui/icons-material";
-import { useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+import { Button } from "@/components/ui/button"
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
 import axios from "axios";
-import { toast, ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+import {toast, ToastContainer} from "react-toastify";
+import {login} from "@/api/authAPI.ts";
 
-const validationSchema = z.object({
+const formSchema = z.object({
     username: z.string().min(1, "Tên đăng nhập là bắt buộc"),
     password: z
         .string()
-        .min(6, "Mật khẩu phải có ít nhất 6 ký tự")
+        .min(5, "Mật khẩu phải có ít nhất 6 ký tự")
         .min(1, "Mật khẩu là bắt buộc"),
-});
+})
 
-type FormValues = z.infer<typeof validationSchema>;
+export function LoginForm() {
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            username: "",
+            password: "",
+        },
+    })
 
-const Login: React.FC = () => {
-    const [showPassword, setShowPassword] = useState<boolean>(false);
-
-    const { register, handleSubmit, formState: { errors } } = useForm<FormValues>({
-        resolver: zodResolver(validationSchema),
-    });
-
-    const handleLogin = async (values: FormValues) => {
+    const onSubmit = async (values: z.infer<typeof formSchema>) => {
         try {
-            const response = await axios.post(
-                "http://localhost:8888/api/auth/login",
-                { username: values.username, password: values.password },
-                { withCredentials: true }
-            );
+            const data = await login(values.username, values.password);
 
-            localStorage.setItem("access_token", response.data.data.accessToken);
-            localStorage.setItem("accountId", response.data.data.user.accountID);
+            localStorage.setItem("access_token", data.accessToken);
+            localStorage.setItem("accountId", data.user.accountID);
 
             toast.success("Đăng nhập thành công.");
         } catch (error) {
@@ -45,158 +48,58 @@ const Login: React.FC = () => {
             }
         }
     };
-
     return (
-        <Box
-            sx={{
-                width: "100vw",
-                height: "100vh",
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                backgroundColor: "#FFF",
-                color: "#000",
-            }}
-        >
-            <Box
-                sx={{
-                    width: 480,
-                    padding: 4,
-                    backgroundColor: "#FFF",
-                    boxShadow: 3,
-                    borderRadius: 2,
-                    textAlign: "center",
-                    border: "1px solid #E0E0E0",
-                }}
-            >
-                <Typography
-                    variant="h5"
-                    sx={{
-                        fontWeight: 700,
-                        mb: 3,
-                        fontSize: "30px",
-                        lineHeight: "45px",
-                        color: "#333",
-                        fontFamily: "'Roboto', sans-serif",
-                    }}
-                >
-                    Đăng nhập
-                </Typography>
+        <Form {...form}>
+            <div className="flex items-center justify-center w-screen bg-white h-screen">
+                <form onSubmit={form.handleSubmit(onSubmit)}
+                      className="space-y-8 w-[480px] bg-white p-8 rounded-lg shadow-lg">
+                    <div className="text-center space-y-2">
+                        <h1 className="text-2xl font-bold text-gray-800">
+                            Internet Banking
+                        </h1>
+                        <p className="text-sm text-gray-500">
+                            Đăng nhập để tiếp tục sử dụng dịch vụ
+                        </p>
+                    </div>
 
-                <form onSubmit={handleSubmit(handleLogin)} noValidate>
-                    <Typography
-                        sx={{
-                            fontWeight: "bold",
-                            fontSize: "14px",
-                            lineHeight: "21px",
-                            textAlign: "left",
-                            color: "#555",
-                            mb: 0,
-                        }}
-                    >
-                        Tên đăng nhập
-                    </Typography>
-                    <TextField
-                        placeholder="Tên đăng nhập"
-                        type="text"
-                        fullWidth
-                        variant="outlined"
-                        margin="normal"
-                        {...register("username")}
-                        error={Boolean(errors.username)}
-                        helperText={errors.username?.message}
-                        sx={{
-                            "& .MuiFormHelperText-root": {
-                                fontSize: "12px",
-                                color: "#f44336",
-                                marginTop: "5px",
-                                marginLeft: "0px",
-                            },
-                            "& .MuiOutlinedInput-root": {
-                                borderRadius: "8px",
-                                borderColor: "#BDBDBD",
-                            },
-                        }}
+                    <FormField
+                        control={form.control}
+                        name="username"
+                        render={({field}) => (
+                            <FormItem>
+                                <FormLabel className="font-bold">Tên đăng nhập</FormLabel>
+                                <FormControl>
+                                    <Input placeholder="Tên đăng nhập" {...field} />
+                                </FormControl>
+                                <FormMessage/>
+                            </FormItem>
+                        )}
                     />
-
-                    <Typography
-                        sx={{
-                            fontWeight: "bold",
-                            fontSize: "14px",
-                            lineHeight: "21px",
-                            textAlign: "left",
-                            color: "#555",
-                            mb: 0,
-                            mt: 1
-                        }}
-
-                    >
-                        Mật khẩu
-                    </Typography>
-                    <TextField
-                        placeholder="Mật khẩu"
-                        type={showPassword ? "text" : "password"}
-                        fullWidth
-                        variant="outlined"
-                        margin="normal"
-                        {...register("password")}
-                        error={Boolean(errors.password)}
-                        helperText={errors.password?.message}
-                        InputProps={{
-                            endAdornment: (
-                                <InputAdornment position="end">
-                                    <IconButton onClick={() => setShowPassword(!showPassword)} edge="end">
-                                        {showPassword ? <VisibilityOff /> : <Visibility />}
-                                    </IconButton>
-                                </InputAdornment>
-                            ),
-                        }}
-                        sx={{
-                            "& .MuiFormHelperText-root": {
-                                fontSize: "12px",
-                                color: "#f44336",
-                                marginTop: "5px",
-                                marginLeft: "0px",
-                            },
-                            "& .MuiOutlinedInput-root": {
-                                borderRadius: "8px",
-                                borderColor: "#BDBDBD",
-                            },
-                        }}
+                    <FormField
+                        control={form.control}
+                        name="password"
+                        render={({field}) => (
+                            <FormItem>
+                                <FormLabel className="font-bold">Mật khẩu</FormLabel>
+                                <FormControl>
+                                    <Input placeholder="Mật khẩu" {...field} />
+                                </FormControl>
+                                <FormMessage/>
+                            </FormItem>
+                        )}
                     />
-                    <Box sx={{ textAlign: "left", mt: 1 }}>
-                        <Link href="#" variant="body2" color="primary">
+                    <Button className="w-full hover:border-none" type="submit">Đăng nhập</Button>
+                    <div className="text-left">
+                        <a
+                            href="/forgot-password"
+                            className="text-sm"
+                        >
                             Quên mật khẩu?
-                        </Link>
-                    </Box>
-
-                    <Button
-                        fullWidth
-                        variant="contained"
-                        color="primary"
-                        sx={{
-                            mt: 3,
-                            textTransform: "none",
-                            fontWeight: 600,
-                            fontSize: "16px",
-                            padding: "12px 0",
-                            borderRadius: "8px",
-                            backgroundColor: "#333",
-                            "&:focus": {
-                                outline: "none",
-                                boxShadow: "none",
-                            },
-                        }}
-                        type="submit"
-                    >
-                        Đăng nhập
-                    </Button>
+                        </a>
+                    </div>
                 </form>
-            </Box>
-
-            <ToastContainer />
-        </Box>
-    );
-};
-
-export default Login;
+            </div>
+            <ToastContainer/>
+        </Form>
+    )
+}

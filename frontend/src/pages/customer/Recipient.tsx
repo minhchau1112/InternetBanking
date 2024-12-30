67cf00d1c18e990b8f22a83a4b393b4325cce533
dinhhuyhuy <tdhuy21@clc.fitus.edu.vn>
2024-12-31 00:47:02 +0700

    Update recipient table ui

--- frontend/src/pages/customer/Recipient.tsx
+++ frontend/src/pages/customer/Recipient.tsx
@@ -1,121 +1,136 @@
-import { Edit, Delete } from '@mui/icons-material';
+import { Edit, Delete, NavigateBefore, NavigateNext } from '@mui/icons-material';
 
 
 const Recipient = () => {
     return (
-        <div className="min-h-screen w-full bg-gray-100 p-6">
-            <div className="flex-col justify-between">
-                <h1 className="text-2xl font-bold mb-2">Customer Recipients</h1>
-                <h2 className="text-lg font-light mb-10">Manage your recipients</h2>
-                <div className="min-h-full w-3/5 rounded-2xl bg-white p-6">
-                    <h3 className="text-3xl font-bold mb-4">Recipients</h3>
-                    <div className="w-full p-[2%] border border-gray-200 rounded-2xl">
-                        <table className="w-full text-lg text-left border-collapse">
+        <div className="w-[calc(100vw-256px)] max-w-full h-screen flex items-center justify-center p-8 bg-gray-100 ">
+            <div className="w-full h-full bg-white p-8 rounded-lg shadow-md">
+                <h1 className="text-3xl font-bold mb-6 text-gray-800 text-left">Manage Customer Recipients</h1>
+                <div className="w-full grid grid-cols-3">
+                    <div className="w-full h-full flex items-center justify-center col-span-2">
+                        <div className="w-full p-[2%] border border-gray-200 rounded-3xl">
+                        <table className="w-full text-lg text-left p-[2%] border-collapse">
                             <thead>
-                            <tr>
-                                <th className="py-[2%] px-[3%] w-[30%] text-blue-600">Alias Name</th>
-                                <th className="py-[2%] px-[3%] w-[30%] text-blue-600">Account Number</th>
-                                <th className="py-[2%] px-[3%] w-[20%] text-blue-600">Bank Code</th>
-                                <th className="py-[2%] px-[3%] w-[20%]"></th>
+                            <tr className="border-b">
+                                <th className="py-[2%] w-[40%] font-bold">Alias Name</th>
+                                <th className="py-[2%] w-[30%] font-bold">Account Number</th>
+                                <th className="py-[2%] w-[20%] font-bold">Bank Code</th>
+                                <th className="py-[2%] w-[10%]"></th>
                             </tr>
                             </thead>
                             <tbody>
-                            <tr>
-                                <td className="py-[2%] px-[3%]">Alias Name</td>
-                                <td className="py-[2%] px-[3%] text-gray-400">1xxxxxxxxxx0</td>
-                                <td className="py-[2%] px-[3%]">HBS</td>
-                                <td className="py-[2%] px-[3%] flex">
+                            <tr className="border-b">
+                                <td className="py-[2%]">Alias Name</td>
+                                <td className="py-[2%] text-gray-400">1xxxxxxxxxx0</td>
+                                <td className="py-[2%]">HBS</td>
+                                <td className="py-[2%] flex">
                                     <button className="bg-transparent p-2"><Edit/></button>
                                     <button className="bg-transparent p-2"><Delete/></button>
                                 </td>
                             </tr>
-                            <tr>
-                                <td className="py-[2%] px-[3%]">Fadhil Aksara</td>
-                                <td className="py-[2%] px-[3%] text-gray-400">1xxxxxxxxxx0</td>
-                                <td className="py-[2%] px-[3%]">HBS</td>
-                                <td className="py-[2%] px-[3%] flex">
+                            <tr className="border-b">
+                                <td className="py-[2%]">Fadhil Aksara</td>
+                                <td className="py-[2%] text-gray-400">1xxxxxxxxxx0</td>
+                                <td className="py-[2%]">HBS</td>
+                                <td className="py-[2%] flex">
                                     <button className="bg-transparent p-2"><Edit/></button>
                                     <button className="bg-transparent p-2"><Delete/></button>
                                 </td>
                             </tr>
-                            <tr>
-                                <td className="py-[2%] px-[3%]">Fadhil Rubian</td>
-                                <td className="py-[2%] px-[3%] text-gray-400">1xxxxxxxxxx0</td>
-                                <td className="py-[2%] px-[3%]">HBS</td>
-                                <td className="py-[2%] px-[3%] flex">
+                            <tr className="border-b">
+                                <td className="py-[2%]">Fadhil Rubian</td>
+                                <td className="py-[2%] text-gray-400">1xxxxxxxxxx0</td>
+                                <td className="py-[2%]">HBS</td>
+                                <td className="py-[2%] flex">
                                     <button className="bg-transparent p-2"><Edit/></button>
                                     <button className="bg-transparent p-2"><Delete/></button>
                                 </td>
                             </tr>
-                            <tr>
-                                <td className="py-[2%] px-[3%]">Fadhil Gimari</td>
-                                <td className="py-[2%] px-[3%] text-gray-400">1xxxxxxxxxx0</td>
-                                <td className="py-[2%] px-[3%]">HBS</td>
-                                <td className="py-[2%] px-[3%] flex">
+                            <tr className="border-b">
+                                <td className="py-[2%]">Fadhil Gimari</td>
+                                <td className="py-[2%] text-gray-400">1xxxxxxxxxx0</td>
+                                <td className="py-[2%]">HBS</td>
+                                <td className="py-[2%] flex">
                                     <button className="bg-transparent p-2"><Edit/></button>
                                     <button className="bg-transparent p-2"><Delete/></button>
                                 </td>
                             </tr>
-                            <tr>
-                                <td className="py-[2%] px-[3%]">Fadhil Muhammad</td>
-                                <td className="py-[2%] px-[3%] text-gray-400">1xxxxxxxxxx0</td>
-                                <td className="py-[2%] px-[3%]">HBS</td>
-                                <td className="py-[2%] px-[3%] flex">
+                            <tr className="border-b">
+                                <td className="py-[2%]">Fadhil Muhammad</td>
+                                <td className="py-[2%] text-gray-400">1xxxxxxxxxx0</td>
+                                <td className="py-[2%]">HBS</td>
+                                <td className="py-[2%] flex">
                                     <button className="bg-transparent p-2"><Edit/></button>
                                     <button className="bg-transparent p-2"><Delete/></button>
                                 </td>
                             </tr>
-                            <tr>
-                                <td className="py-[2%] px-[3%]">Fadhil Sausu</td>
-                                <td className="py-[2%] px-[3%] text-gray-400">1xxxxxxxxxx0</td>
-                                <td className="py-[2%] px-[3%]">HBS</td>
-                                <td className="py-[2%] px-[3%] flex">
+                            <tr className="border-b" >
+                                <td className="py-[2%]">Fadhil Sausu</td>
+                                <td className="py-[2%] text-gray-400">1xxxxxxxxxx0</td>
+                                <td className="py-[2%]">HBS</td>
+                                <td className="py-[2%] flex">
                                     <button className="bg-transparent p-2"><Edit/></button>
                                     <button className="bg-transparent p-2"><Delete/></button>
                                 </td>
                             </tr>
-                            <tr>
-                                <td className="py-[2%] px-[3%]">Fadhil Sausu</td>
-                                <td className="py-[2%] px-[3%] text-gray-400">1xxxxxxxxxx0</td>
-                                <td className="py-[2%] px-[3%]">HBS</td>
-                                <td className="py-[2%] px-[3%] flex">
+                            <tr className="border-b">
+                                <td className="py-[2%]">Fadhil Sausu</td>
+                                <td className="py-[2%] text-gray-400">1xxxxxxxxxx0</td>
+                                <td className="py-[2%]">HBS</td>
+                                <td className="py-[2%] flex">
                                     <button className="bg-transparent p-2"><Edit/></button>
                                     <button className="bg-transparent p-2"><Delete/></button>
                                 </td>
                             </tr>
-                            <tr>
-                                <td className="py-[2%] px-[3%]">Fadhil Sausu</td>
-                                <td className="py-[2%] px-[3%] text-gray-400">1xxxxxxxxxx0</td>
-                                <td className="py-[2%] px-[3%]">HBS</td>
-                                <td className="py-[2%] px-[3%] flex">
+                            <tr className="border-b">
+                                <td className="py-[2%]">Fadhil Sausu</td>
+                                <td className="py-[2%] text-gray-400">1xxxxxxxxxx0</td>
+                                <td className="py-[2%]">HBS</td>
+                                <td className="py-[2%] flex">
                                     <button className="bg-transparent p-2"><Edit/></button>
                                     <button className="bg-transparent p-2"><Delete/></button>
                                 </td>
                             </tr>
-                            <tr>
-                                <td className="py-[2%] px-[3%]">Fadhil Sausu</td>
-                                <td className="py-[2%] px-[3%] text-gray-400">1xxxxxxxxxx0</td>
-                                <td className="py-[2%] px-[3%]">HBS</td>
-                                <td className="py-[2%] px-[3%] flex">
+                            <tr className="border-b">
+                                <td className="py-[2%]">Fadhil Sausu</td>
+                                <td className="py-[2%] text-gray-400">1xxxxxxxxxx0</td>
+                                <td className="py-[2%]">HBS</td>
+                                <td className="py-[2%] flex">
                                     <button className="bg-transparent p-2"><Edit/></button>
                                     <button className="bg-transparent p-2"><Delete/></button>
                                 </td>
                             </tr>
                             </tbody>
                         </table>
-                        <div className="flex justify-center mt-[2%] space-x-[1%]">
-                            <button className="py-[1%] px-[2%] bg-yellow-400 rounded-full">1</button>
-                            <button className="py-[1%] px-[2%] bg-gray-200 rounded-full">2</button>
-                            <button className="py-[1%] px-[2%] bg-gray-200 rounded-full">3</button>
-                            <button className="py-[1%] px-[2%] bg-gray-200 rounded-full">4</button>
-                            <button className="py-[1%] px-[2%] bg-gray-200 rounded-full">5</button>
-                            <button className="py-[1%] px-[2%] bg-gray-200 rounded-full">&gt;</button>
+                            <div className="flex justify-center items-center mt-[2%] space-x-[1.5%]">
+                                <button className="py-1 px-1 bg-black rounded-full"><NavigateBefore style={{ color: "white", fontSize: 30 }}/></button>
+                                <p className="text-lg">Page 1/10</p>
+                                <button className="py-1 px-1 bg-black rounded-full"><NavigateNext style={{ color: "white", fontSize: 30 }}/></button>
+                            </div>
                         </div>
                     </div>
-
                 </div>
             </div>
         </div>
+        // <div className="min-h-screen w-full bg-gray-100 p-6">
+        //     <div className="flex-col justify-between">
+        //         <div className="min-h-full w-3/5 rounded-2xl bg-white p-6">
+        //             <h3 className="text-3xl font-bold mb-4">Recipients</h3>
+        //             <div className="w-full p-[2%] border border-gray-200 rounded-2xl">
+
+        //                 <div className="flex justify-center mt-[2%] space-x-[1%]">
+        //                     <button className="py-[1%] px-[2%] bg-yellow-400 rounded-full">1</button>
+        //                     <button className="py-[1%] px-[2%] bg-gray-200 rounded-full">2</button>
+        //                     <button className="py-[1%] px-[2%] bg-gray-200 rounded-full">3</button>
+        //                     <button className="py-[1%] px-[2%] bg-gray-200 rounded-full">4</button>
+        //                     <button className="py-[1%] px-[2%] bg-gray-200 rounded-full">5</button>
+        //                     <button className="py-[1%] px-[2%] bg-gray-200 rounded-full">&gt;</button>
+        //                 </div>
+        //             </div>
+        //
+        //         </div>
+        //     </div>
+        // </div>
     )
 }
 

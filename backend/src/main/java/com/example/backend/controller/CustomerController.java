38bf58158ee8c0340616b7f0d455147fd88c5259
Le Tuan Binh <ltbinh21@clc.fitus.edu.vn>
2024-12-31 22:20:02 +0700

    Fix the role permission in security

--- backend/src/main/java/com/example/backend/controller/CustomerController.java
+++ backend/src/main/java/com/example/backend/controller/CustomerController.java
@@ -1,4 +1,25 @@
 package com.example.backend.controller;
 
+
+import com.example.backend.model.Customer;
+import com.example.backend.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class CustomerController {
+
+    private final CustomerService customerService;
+
+    @Autowired
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    @GetMapping("/customer")
+    public Customer getCustomerDetailsByUsername(@RequestParam String username) {
+        return customerService.getCustomerByUsername(username);
+    }
 }

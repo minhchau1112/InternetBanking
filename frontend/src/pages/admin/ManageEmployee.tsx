b16b468d532f061b97b6458e98a63bf9b555c031
FelinTran <felintran.work@gmail.com>
2025-01-07 00:08:47 +0700

    [FE] Add Manage Employee page [BE] Add Employee Sevice.java

---
+++ frontend/src/pages/admin/ManageEmployee.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ManageEmployee = () => {
+    return (
+        <div className="w-[calc(100vw-256px)] max-w-full h-screen flex items-center justify-center p-8 bg-gray-100 ">
+            <div className="w-full bg-white p-8 rounded-lg shadow-md">
+                <h1 className="text-3xl font-bold mb-6 text-gray-800 text-left">Manage Employee</h1>
+
+        </div>
+    );
+};
+
+export default ManageEmployee;
